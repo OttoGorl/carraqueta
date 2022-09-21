7df927c2c6612744dcc4bdeed6ec834b18bccf4b
otto alberto gorl <otto.gorl@cwi.com.br>
2022-09-21 19:42:15 -0300

    initial commit

---
+++ carraqueta/src/main/java/com/carraqueta/carraqueta/CarraquetaApplication.java
@@ -0,0 +1,13 @@
+package com.carraqueta.carraqueta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarraquetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarraquetaApplication.class, args);
+	}
+
+}
---
+++ carraqueta/src/test/java/com/carraqueta/carraqueta/CarraquetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carraqueta.carraqueta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarraquetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
